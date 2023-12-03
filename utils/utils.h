9912fb787a8f960f84ae1af6d7d4d1e67dd82db6
i-run-knee <i.run.knee@protonmail.com>
2023-12-02 18:28:28 -0800

    day 1 complete

--- utils/utils.h
+++ utils/utils.h
@@ -1,3 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <fstream>
 #include <string>
 #include <sstream>
@@ -5,10 +8,12 @@
 namespace utils
 {
     void writeToFile(std::string filename, std::string content);
-    std::string readFromFile(std::string filename);
+    void readFromFile(std::string filename);
     std::string intToString(int number);
     int stringToInt(std::string str);
-    
+    int charToInt(char c);
     template<class UnaryFunction>
     UnaryFunction for_each_line(std::string filename, UnaryFunction f);
 }
+
+#endif // UTILS_H
